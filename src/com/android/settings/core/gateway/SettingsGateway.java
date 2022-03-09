6b78cf299e158f088c5c9baf2cf225d44487be34
xyzuan <xyzuannihboss@gmail.com>
2022-03-09 16:56:30 +0800

    Settings: grimoire: init grimoire
    Co-authored-by: maxwen <max.weninger@gmail.com>
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -167,6 +167,7 @@ import com.android.settings.wifi.details.WifiNetworkDetailsFragment;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
+import com.arcana.grimoire.Dashboard;
 
 public class SettingsGateway {
 
@@ -323,7 +324,8 @@ public class SettingsGateway {
             NetworkProviderSettings.class.getName(),
             NetworkSelectSettings.class.getName(),
             AlarmsAndRemindersDetails.class.getName(),
-            MediaManagementAppsDetails.class.getName()
+            MediaManagementAppsDetails.class.getName(),
+            Dashboard.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -364,5 +366,6 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            Settings.grimoireActivity.class.getName(),
     };
 }
