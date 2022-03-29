d7dbc6b40def7d6c2037757b1feaacc9b52c5240
dlwlrma123 <alexfinhart@gmail.com>
2022-03-30 06:00:06 +0800

    grimoire: misc statusbar fragment cleanup
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- src/com/arcana/grimoire/fragments/MiscStatusbar.java
+++ src/com/arcana/grimoire/fragments/MiscStatusbar.java
@@ -28,10 +28,14 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
 import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import org.arcana.support.preference.SecureSettingSwitchPreference;
+import org.arcana.support.preference.SystemSettingSwitchPreference;
+import org.arcana.support.preference.SystemSettingListPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.arcana.ArcanaUtils;
 
 import java.util.Locale;
 import java.util.regex.Matcher;
@@ -43,7 +47,17 @@ public class MiscStatusbar extends SettingsPreferenceFragment implements OnPrefe
     private static final String COMBINED_STATUSBAR_ICONS = "show_combined_status_bar_signal_icons";
     private static final String CONFIG_RESOURCE_NAME = "flag_combined_status_bar_signal_icons";
     private static final String SYSTEMUI_PACKAGE = "com.android.systemui";
+    private static final String KEY_SHOW_VOLTE = "show_volte_icon";
+    private static final String KEY_SHOW_VOWIFI = "show_vowifi_icon";
+    private static final String KEY_SHOW_FOURG = "show_fourg_icon";
+    private static final String KEY_SHOW_DATA_DISABLED = "data_disabled_icon";
+    private static final String KEY_USE_OLD_MOBILETYPE = "use_old_mobiletype";
 
+    private SystemSettingSwitchPreference mShowFourg;
+    private SystemSettingSwitchPreference mDataDisabled;
+    private SystemSettingSwitchPreference mOldMobileType;
+    private SystemSettingSwitchPreference mShowVolte;
+    private SystemSettingListPreference mShowVowifi;
     SecureSettingSwitchPreference mCombinedIcons;
 
     @Override
@@ -53,6 +67,8 @@ public class MiscStatusbar extends SettingsPreferenceFragment implements OnPrefe
         addPreferencesFromResource(R.xml.grimoire_misc_statusbar);
         
         final ContentResolver resolver = getActivity().getContentResolver();
+        final Context mContext = getActivity().getApplicationContext();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
         
         // combined signal icons
 	mCombinedIcons = (SecureSettingSwitchPreference)
@@ -76,6 +92,20 @@ public class MiscStatusbar extends SettingsPreferenceFragment implements OnPrefe
         mCombinedIcons.setChecked(enabled);
         mCombinedIcons.setOnPreferenceChangeListener(this);
         // combined signal icons
+
+        mShowVolte = (SystemSettingSwitchPreference) findPreference(KEY_SHOW_VOLTE);
+        mShowVowifi = (SystemSettingListPreference) findPreference(KEY_SHOW_VOWIFI);
+        mShowFourg = (SystemSettingSwitchPreference) findPreference(KEY_SHOW_FOURG);
+        mDataDisabled = (SystemSettingSwitchPreference) findPreference(KEY_SHOW_DATA_DISABLED);
+        mOldMobileType = (SystemSettingSwitchPreference) findPreference(KEY_USE_OLD_MOBILETYPE);
+        
+        if (!ArcanaUtils.isVoiceCapable(getActivity())) {
+            prefScreen.removePreference(mShowVolte);
+            prefScreen.removePreference(mShowVowifi);
+            prefScreen.removePreference(mShowFourg);
+            prefScreen.removePreference(mDataDisabled);
+            prefScreen.removePreference(mOldMobileType);
+        }
         
     }
 
