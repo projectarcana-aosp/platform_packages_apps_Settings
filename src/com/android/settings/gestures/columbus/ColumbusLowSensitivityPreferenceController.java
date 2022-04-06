PixelExperience/packages_apps_Settings

ColumbusLowSensitivityPreferenceController: Disable if using ap sensor
Change-Id: I34ea6a84a427eced3764c0cdd8347ea0f01c16c0

src/org/pixelexperience/settings/gestures/columbus/ColumbusLowSensitivityPreferenceController.java
@@ -1,0 +1,22 @@
+package org.pixelexperience.settings.gestures.columbus;
+
+import android.content.Context;
+import android.content.IntentFilter;
+import com.android.settings.slices.SliceBackgroundWorker;
+import com.google.android.settings.gestures.columbus.ColumbusTogglePreferenceController;
+
+import android.os.SystemProperties;
+
+public class ColumbusLowSensitivityPreferenceController extends ColumbusTogglePreferenceController {
+    public ColumbusLowSensitivityPreferenceController(Context context, String str) {
+        super(context, str, 1747);
+    }
+
+    @Override // com.android.settings.core.BasePreferenceController
+    public int getAvailabilityStatus() {
+        if (SystemProperties.getBoolean("persist.columbus.use_ap_sensor", true)){
+            return UNSUPPORTED_ON_DEVICE;
+        }
+        return super.getAvailabilityStatus();
+    }
+}

projectarcana-aosp/platform_packages_apps_Settings

Change-Id: I0f0fe01c1e92860270c956107a1fca64f6e0de08
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/settings/gestures/columbus/ColumbusLowSensitivityPreferenceController.java
@@ -1,0 +1,22 @@
+package com.android.settings.gestures.columbus;
+
+import android.content.Context;
+import android.content.IntentFilter;
+import com.android.settings.slices.SliceBackgroundWorker;
+import com.google.android.settings.gestures.columbus.ColumbusTogglePreferenceController;
+
+import android.os.SystemProperties;
+
+public class ColumbusLowSensitivityPreferenceController extends ColumbusTogglePreferenceController {
+    public ColumbusLowSensitivityPreferenceController(Context context, String str) {
+        super(context, str, 1747);
+    }
+
+    @Override // com.android.settings.core.BasePreferenceController
+    public int getAvailabilityStatus() {
+        if (SystemProperties.getBoolean("persist.columbus.use_ap_sensor", true)){
+            return UNSUPPORTED_ON_DEVICE;
+        }
+        return super.getAvailabilityStatus();
+    }
+}